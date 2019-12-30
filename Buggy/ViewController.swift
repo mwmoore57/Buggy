0d6b015296127af3ed124c87b5c43ea22cbf9b3b
MichaelWMoore <mwmoore808@hotmail.com>
2019-12-30 15:25:06 -0700

    A very simple app to demonstrate various debugging techniques. Including print(), breakpoints and LLDB console

--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Method: \(#function) in file: \(#file) line: \(#line)")
+        // log sender
+        print("sender: \(sender)")
+        
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        // Go one step too far emptying the array (notice the range change):
+        // Bug found and fixed
+        for j in 0..<10 {
+            print(array[j])
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
